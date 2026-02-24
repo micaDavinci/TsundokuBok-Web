a245d2f37caa0c3ad4cc179bb0015887abda110b
Micaela Soto <micaela.soto@davinci.edu.ar>
2026-02-24 17:58:21 -0300

    busqueda con googleBook

--- src/pages/NuevoLibro/BuscarLibro.jsx
+++ src/pages/NuevoLibro/BuscarLibro.jsx
@@ -3,12 +3,20 @@ import { Resultado } from "./Resultado"
 import { Button, Form, Row } from "react-bootstrap"
 
 export const BuscarLibro = () => {
+    const [query, setQuery] = useState("");
+    const [books, setBooks] = useState([]);
+
+    const buscarLibro = async () => {
+        const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`);
+        const data = await res.json();
+        setBooks(data.items || []);
+    }
     return (
         <>
             <h1 className='mb-4'>Buscar libro</h1>
 
             <Row md={3} lg={3}>
-                <Form className="d-flex">
+                {/* <Form className="d-flex">
                     <Form.Control
                         type="search"
                         placeholder="Search"
@@ -16,18 +24,26 @@ export const BuscarLibro = () => {
                         aria-label="Search"
                     />
                     <Button variant="outline-secondary"><i class="bi bi-search"></i></Button>
-                </Form>
+                </Form> */}
+
+                <Input
+                    type="text"
+                    placeholder="Buscar un libro"
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                    style={{ padding: '0.5rem', width: '300px', marginRight: '1rem' }}>
+                </Input>
+                <Button variant="outline-secondary" onClick={buscarLibro}>
+                    <i className="bi bi-search">Buscar</i>
+                </Button>
             </Row>
 
             <Link to="/mi-biblioteca/nuevo-libro" className="d-flex justify-content-end mt-2 link-rosa"><i class="bi bi-chevron-left"> Volver</i></Link>
 
             <Row xs={3} md={4} className="g-3 mt-1">
-                <Resultado />
-                <Resultado />
-                <Resultado />
-                <Resultado />
-                <Resultado />
-                <Resultado />
+                {books.map( book => (
+                    <Resultado key={book.id} book={book.volumeInfo}/>
+                ))};
             </Row>
         </>
     )
--- src/pages/NuevoLibro/Resultado.jsx
+++ src/pages/NuevoLibro/Resultado.jsx
@@ -1,13 +1,18 @@
 import { Card, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
-export const Resultado = () => {
+export const Resultado = (book) => {
+     const { title, authors, imageLinks, description, infoLink } = book;
     return (
         <Col sm={4} md={3} lg={3}>
             <Card className="p-2">
                 <Link className="link-offset-2 link-underline link-underline-opacity-0 color-rosaT" to="/mi-biblioteca/agregar-libro">
-                    <Card.Img variant="top" src="../img/img.jpg" className="img-fluidrounded-start"></Card.Img>
-                    <Card.Title className="mt-2">Título</Card.Title>
+                    <Card.Img variant="top" src={imageLinks.thumbnail} className="img-fluidrounded-start"></Card.Img>
+                    <Card.Title className="mt-2">{title}</Card.Title>
+                    <Card.Text>{authors?.join(', ')}</Card.Text>
+                    <Card.Text>{description}</Card.Text>
+                    <Link to={infoLink}> Ver más</Link>
+
                 </Link>
             </Card>
         </Col>
